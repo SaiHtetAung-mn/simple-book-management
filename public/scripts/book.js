15dc8b9c5cdf1ee5d4acf68bfb2a8d70bb3e1ed7
Sai Htet Aung <saihtetaung276@gmail.com>
2022-01-26 22:23:23 +0630

    Simple book directory v1.0 all ok

--- public/scripts/book.js
+++
@@ -1,6 +0,0 @@
-export default function Book(book={}) {
-    this.isbn = book.isbn ?? '';
-    this.title = book.title ?? '';
-    this.author = book.author ?? '';
-    this.category = book.category ?? '';
-}
